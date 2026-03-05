03fa04e8b95669ab2fa8b70ca2d261582e7c1441
Kinjal Kathrecha <kinjalkathrecha2@gmail.com>
2026-03-05 12:32:05 +0530

    create app + consumers + routing

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'chat',
+    'channels',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+ASGI_APPLICATION = "config.asgi.application"
