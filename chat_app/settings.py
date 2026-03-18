446d1e0b39d791d8170bcd972e78282f4f19e116
Kinjal Kathrecha <kinjalkathrecha2@gmail.com>
2026-03-18 14:57:19 +0530

    setup redis

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -118,7 +118,10 @@ LOGOUT_REDIRECT_URL = 'index'
 # Channels Configuration
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
     },
 }
 
